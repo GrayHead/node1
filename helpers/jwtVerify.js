6090a701722c4cd3d6fb80e840ec36df7321e77e
GrayHead <ss.zhuravlov@gmail.com>
2019-11-26 17:35:37 +0200

    add jwt

--- helpers/jwtVerify.js
+++ helpers/jwtVerify.js
@@ -3,7 +3,7 @@ let {jwt_secret} = require('../constants/config');
 module.exports = (token) => {
     jwt.verify(token, jwt_secret, (error) => {
         if (error) {
-            throw new Error('not valid token');
+            throw new Error('invalid token');
         }
     })
 
